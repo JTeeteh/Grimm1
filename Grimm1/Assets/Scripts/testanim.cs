7cf09939a064841ad0cf9232691bb830967c14db
JTor355 <torres.joshua333@gmail.com>
2023-11-19 01:42:35 +0800

    stage map update
    - camera size
    - player (NPC)
    - monster anim update

---
+++ Grimm1/Assets/Scripts/testanim.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testanim : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            this.GetComponent<Animator>().Play("attack");
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            this.GetComponent<Animator>().Play("summon");
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            this.GetComponent<Animator>().Play("skill1");
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            //this.GetComponent<Animator>().Play("dead");
+        }
+    }
+
+    void kys()
+    {
+        Destroy(this.gameObject);
+    }
+
+}
