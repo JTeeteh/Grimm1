6fc764106d6675814fa6b1ede2e74729a06b8d4d
JTor355 <torres.joshua333@gmail.com>
2023-11-19 10:49:21 +0800

    code modification
    -weapon script is deleted ( not really )
    
    - weapon script is part of the playermovement script so it can be controlled via PUN 2

--- Grimm1/Assets/Scripts/Z1_Script/PlayerMovement.cs
+++ Grimm1/Assets/Scripts/Z1_Script/PlayerMovement.cs
@@ -16,21 +16,42 @@ public class PlayerMovement : MonoBehaviour
 
     public static int life = 5;
 
-    private void start()
+    // Weapon-related variables
+    public Transform firePoint, firePointCrouch;
+    private GameObject bulletPrefab;
+    public GameObject[] bullet;
+    public GameObject[] gun;
+
+    private float nextTimeOfFire = 0;
+    public float fireRate;
+    private bool canFire;
+    private bool timerStarted;
+
+    private void Start()
     {
-        life = healthController.hearts.Length;
+        //life = healthController.hearts.Length;
+
+        // Weapon initialization
+        fireRate = 10f;
+        bulletPrefab = bullet[0];
+        canFire = true;
+        timerStarted = false;
     }
-    // Update is called once per frame
+
     void Update()
     {
+        // Player movement input
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
         animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
+        // Jumping input
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
             animator.SetBool("IsJumping", true);
         }
+
+        // Crouch input
         if (Input.GetButtonDown("Crouch"))
         {
             crouch = true;
@@ -40,12 +61,22 @@ public class PlayerMovement : MonoBehaviour
             crouch = false;
         }
 
+        // Update animator for crouching
+        onCrouching(crouch);
+
+        // Firing input
+        if (Input.GetButton("Fire1") && canFire)
+        {
+            StartCoroutine(Fire());
+        }
+
     }
 
     public void OnLanding()
     {
         animator.SetBool("IsJumping", false);
     }
+
     public void onCrouching(bool isCrouching)
     {
         animator.SetBool("IsCrouching", isCrouching);
@@ -53,17 +84,15 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        //move character
+        // Move character
         controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false;
     }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
         {
-            //life -= d;
-            //Destroy(hearts[life].gameObject);
-            //SceneManager.LoadScene("MainMenu");
             if (canBeDamaged)
             {
                 healthController.TakeDamage(1);
@@ -86,15 +115,55 @@ public class PlayerMovement : MonoBehaviour
         canBeDamaged = true;
     }
 
-    //public void TakeDamage(int d)
-    //{
-    //    life = life - d;
+    void Shoot(bool isCrouching)
+    {
+        if (!isCrouching)
+            Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        else
+            Instantiate(bulletPrefab, firePointCrouch.position, firePointCrouch.rotation);
+    }
 
+    IEnumerator Fire()
+    {
+        canFire = false;
+        Shoot(crouch);
+        yield return new WaitForSeconds(1f / fireRate);
+        canFire = true;
+    }
 
-    //    if (life < 1)
-    //    {
-    //        SceneManager.LoadScene("MainMenu");
-    //    }
-    //}
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.tag == "Weapon")
+        {
+            // Weapon pickup logic
+            if (target.name == gun[0].name)
+            {
+                bulletPrefab = bullet[0];
+                fireRate = 10f;
+            }
+            else if (target.name == gun[1].name)
+            {
+                bulletPrefab = bullet[1];
+                fireRate = 5f;
+            }
+            else if (target.name == gun[2].name)
+            {
+                bulletPrefab = bullet[2];
+                fireRate = 25f;
+            }
+            else if (target.name == gun[3].name)
+            {
+                bulletPrefab = bullet[3];
+                fireRate = 20f;
+            }
+            else if (target.name == gun[4].name)
+            {
+                bulletPrefab = bullet[4];
+                fireRate = 0.5f;
+            }
 
+            Debug.Log("Gun pickup - upgrade.");
+            Destroy(target.gameObject);
+        }
+    }
 }
